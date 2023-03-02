314d9f74477b01ef85f98902b72a7984fd2b353a
Riku-mono <riitkou@gmail.com>
2023-03-02 21:27:50 +0900

    commit

--- js/app.js
+++ js/app.js
@@ -1,32 +1,61 @@
-// windowのscrollイベントを監視して、スクロール位置が0の時にbodyにpageTopクラスを付与する
-document.addEventListener('DOMContentLoaded', pageTopToggle);
-
-window.addEventListener('scroll', pageTopToggle);
-
+// スクロール位置を保存するコールバック関数
+function saveScrollPosition() {
+    sessionStorage.setItem('scrollPosition', window.scrollY);
+}
+// スクロール位置が0の時にbodyにpageTopクラスを付与するコールバック関数
 function pageTopToggle() {
     var body = document.querySelector('body');
     body.classList.toggle('pageTop', window.scrollY === 0);
-}
+} 
 
-// 
+// 初期化関数
+function init() {
+    pageTopToggle();
+    
+    var scrollPosition = sessionStorage.getItem('scrollPosition');
+    if (scrollPosition !== null) {
+        window.scrollTo(0, parseInt(scrollPosition));
+    }
+
+    setTimeout(() => {
+        let scrollTimer;
+        window.addEventListener('scroll', function() {
+            if(scrollTimer !== null){
+                clearTimeout(scrollTimer);
+            }
+            scrollTimer = setTimeout(function() {
+                pageTopToggle();
+                saveScrollPosition();
+            }, 10);
+        });
+    }, 200);
+};
+
+// ページ読み込み時に実行
+document.addEventListener('DOMContentLoaded', init());
+
+
+
+
+
+// ハンバーガーメニュー
 const navButton = document.querySelector('.nav-toggle');
 navButton.addEventListener('click', function() {
     document.body.classList.toggle('nav-open');
 });
 
-// スクロール位置を保存するコールバック関数
-function saveScrollPosition() {
-    sessionStorage.setItem('scrollPosition', window.scrollY);
-  }
-  
-  // ページ読み込み時にスクロール位置を復元する
-  window.onload = function() {
-    var scrollPosition = sessionStorage.getItem('scrollPosition');
-    if (scrollPosition !== null) {
-        window.scrollTo(0, parseInt(scrollPosition));
+// resizeイベントの負荷軽減
+let resizeTimer;
+window.addEventListener('resize', function() {
+    if(resizeTimer !== null){
+        clearTimeout(resizeTimer);
     }
-  }
-  setTimeout(() => {
-      // スクロール時にスクロール位置を保存する
-      window.addEventListener('scroll', saveScrollPosition);
-  }, 10);
+
+    resizeTimer = setTimeout(function() {
+        // 画面幅が900px以上の時にnav-openクラスを削除
+        if (window.innerWidth >= 900) {
+            document.body.classList.remove('nav-open');
+        }
+    }, 100);
+});
+
