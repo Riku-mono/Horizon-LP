24ea74be9e8ee370b7d313d7a7fef3063578c4fe
Riku-mono <riitkou@gmail.com>
2023-03-11 22:48:40 +0900

    commit

--- js/app.js
+++ js/app.js
@@ -14,11 +14,9 @@ function navAnimation() {
     let startpoint = document.querySelector('.MV-message').offsetTop;
     let scroll = window.pageYOffset;
     if (startpoint > scroll || 0 > scroll - beforeScroll) {
-        body.classList.add('DownMove');
-        body.classList.remove('UpMove');
+        body.classList.remove('is-hide');
     } else {
-        body.classList.add('UpMove');
-        body.classList.remove('DownMove');
+        if(!body.classList.contains('nav-open')) body.classList.add('is-hide');
     }
     beforeScroll = scroll;
 }
@@ -69,6 +67,7 @@ function scrollAnimation() {
 // 初期化関数
 function init() {
     pageTopToggle();
+    newsTicker();
     
     var scrollPosition = sessionStorage.getItem('scrollPosition');
     if (scrollPosition !== null) {
@@ -99,12 +98,13 @@ const body = document.querySelector('body');
 
 // ハンバーガーメニュー
 const navButton = document.querySelector('.nav-toggle');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
+const flyout = document.querySelector('.flyout');
 navButton.addEventListener('click', function() {
     document.body.classList.toggle('nav-open');
-    const main = document.querySelector('main');
-    const footer = document.querySelector('footer');
-    const flyout = document.querySelector('.flyout');
 
+    
     // inert属性で不活性化する
     if(document.body.classList.contains('nav-open')){
         main.inert = true;
@@ -116,14 +116,21 @@ navButton.addEventListener('click', function() {
         flyout.inert = true;
     }
 });
+const flyLinks = document.querySelectorAll('.fly-link-block');
+for (let i = 0; i < flyLinks.length; i++) {
+    flyLinks[i].addEventListener('click', function() {
+        main.inert = false;
+        footer.inert = false;
+        flyout.inert = true;
+    });
+}
 
-// resizeイベントの負荷軽減
+// resizeイベント
 let resizeTimer;
 window.addEventListener('resize', function() {
     if(resizeTimer !== null){
         clearTimeout(resizeTimer);
     }
-
     resizeTimer = setTimeout(function() {
         // 画面幅が900px以上の時にnav-openクラスを削除
         if (window.innerWidth >= 900) {
@@ -139,3 +146,62 @@ flyout_link_titles.forEach(function(flyout_link_title) {
         body.classList.toggle('nav-open');
     });
 });
+
+// newsTicker
+function newsTicker () {
+    const animTime = 5000; // アニメーション時間(ms)
+    const speed = 100; // 移動速度(px)
+    const limit = 0; // ブレークポイント(px)
+    let animId;
+    let isRunning = false;
+
+    const newsTicker = document.querySelector('.newsTicker');
+    const newsTickercontents = newsTicker.querySelector('.newsTicker-contents');
+    loadTicker();
+
+    function loadTicker() {
+        newsTickerAnim();
+        animId = setInterval(newsTickerAnim, animTime);
+        isRunning = true;
+    }
+
+    function newsTickerAnim() {
+        const items = newsTickercontents.querySelectorAll('.newsTicker-item'); 
+        const running = newsTickercontents.querySelector('.run');   
+        let idx, link, first, next;
+        if (!running) {
+            first = items[0];
+            link = first.querySelector('a');
+            first.classList.add('fadeInDown', 'run');
+            first.style.zIndex = 1;
+            setTimeout(textMove, 1000, link);
+        } else {
+            for(let i = 0; i < items.length; i++) {
+                if(items[i].classList.contains('run')) {
+                    idx = i;
+                    break;
+                }
+            }
+            next = items[(idx + 1) % items.length];
+            running.classList.replace('fadeInDown', 'fadeOutDown');
+            setTimeout(() => {
+                running.classList.remove('run', 'fadeOutDown');
+                running.style.zIndex = 0;
+                link = running.querySelector('a');
+                link.style.transform = 'none';
+                next.classList.add('fadeInDown', 'run');
+                next.style.zIndex = 1;
+                link = next.querySelector('a');
+                setTimeout(textMove, 1000, link);
+            }, 1000);
+        }
+    }
+
+    function textMove(link) {
+        const move = link.parentNode.clientWidth - link.clientWidth;
+        if (move < 0) {
+            link.style.transform = `translateX(${move}px)`;
+            link.style.transition = `transform ${Math.abs(move) / speed}s linear`;
+        }
+    }
+}
